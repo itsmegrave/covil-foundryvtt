dfbbe28ad7facf14f8f52abdd25469b37a883c67
Samuel Grave Silva <samuel.gravesilva@buildbook.co>
2023-12-27 14:45:00 -0300

    :sparkles: Change forgeUrl to return only weapons

--- src/module/config.ts
+++ src/module/config.ts
@@ -3,5 +3,5 @@ import { CovilConfig } from '../types/covil-config';
 export const Covil: CovilConfig = {
   logPrefix: 'covil-velho-dragao | ',
   baseUrl: 'https://covil.karlz.com.br/api/',
-  forgeUrl: 'criaitem',
+  forgeUrl: 'criaitem/A', //para retornar apenas armas. depois remover o `/A` para retornar qualquer item
 };
